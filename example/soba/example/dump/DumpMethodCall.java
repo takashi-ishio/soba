eb17a700c072f6b33611ee334e9d49483e4f659d
Takashi Ishio <takashi-ishio@users.osdn.me>
2015-09-04 14:54:11 +0900

    Simplified code example

--- example/soba/example/dump/DumpMethodCall.java
+++ example/soba/example/dump/DumpMethodCall.java
@@ -10,36 +10,24 @@ import soba.util.files.ClasspathUtil;
 public class DumpMethodCall {
 
 	public static void main(String[] args) {
-		long count = 0;
-		long t = System.currentTimeMillis();
-		int classCount = 0;
-		int methodCount = 0;
 		JavaProgram program = new JavaProgram(ClasspathUtil.getClassList(args));
 		ClassHierarchy ch = program.getClassHierarchy();
 		
 		for (ClassInfo c: program.getClasses()) {
-			classCount++;
 			for (MethodInfo m: c.getMethods()) {
-				methodCount++;
 				System.out.println(m.toLongString());
 				for (CallSite cs: m.getCallSites()) {
 					MethodInfo[] callees = ch.resolveCall(cs);
 					if (callees.length > 0) {
 						for (MethodInfo callee: callees) {
-							//System.out.println("  [inside] " + callee.toLongString());
-							count++;
+							System.out.println("  [inside] " + callee.toLongString());
 						}
 					} else {
-						//System.out.println("  [outside] " + cs.toString());
-						count++;
+						System.out.println("  [outside] " + cs.toString());
 					}
 				}
 			}
 		}
-		System.out.println(classCount + " classes");
-		System.out.println(methodCount + " methods");
-		System.out.println(count + " method calls");
-		System.out.println((System.currentTimeMillis() -t ) + "ms");
 	}
 
 }
